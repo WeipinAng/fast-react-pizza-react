5aa3b504aa5c2271603a299efd664b17773af9b2
Weipin Ang <80883634+WeipinAng@users.noreply.github.com>
2025-02-04 01:33:52 +0800

    provide user with some feedback while the form is submitting

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Form, redirect } from "react-router-dom";
+import { Form, redirect, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
@@ -33,6 +33,9 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+    const navigation = useNavigation();
+    const isSubmitting = navigation.state === "submitting";
+
     // const [withPriority, setWithPriority] = useState(false);
     const cart = fakeCart;
 
@@ -80,7 +83,9 @@ function CreateOrder() {
                         name="cart"
                         value={JSON.stringify(cart)}
                     />
-                    <button>Order now</button>
+                    <button disabled={isSubmitting}>
+                        {isSubmitting ? "Placing order..." : "Order now"}
+                    </button>
                 </div>
             </Form>
         </div>
