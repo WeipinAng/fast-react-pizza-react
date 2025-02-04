09c7ecb32c21de363bc7c4a6d661ddda944491d9
Weipin Ang <80883634+WeipinAng@users.noreply.github.com>
2025-02-04 22:41:07 +0800

    tailwind: box model (spacing, border and display)

--- src/features/users/Username.jsx
+++
@@ -1,5 +0,0 @@
-function Username() {
-    return <div className="text-sm font-semibold">Jonas</div>;
-}
-
-export default Username;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
-import Username from "../features/users/Username";
+import Username from "../features/user/Username";
 
 function Header() {
     return (
-        <header className="uppercase bg-yellow-500">
+        <header className="px-4 py-3 uppercase bg-yellow-500 border-b border-stone-200">
             <Link to="/" className="tracking-widest">
                 Fast React Pizza Co.
             </Link>
